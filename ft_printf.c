d928c110c582c080a26b0f5b8a21a61504fe8208
lucafisc <lucafischer@fastmail.com>
2022-12-09 10:24:57 +0100

    Print % signs

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lde-ross <lde-ross@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 17:37:05 by lde-ross          #+#    #+#             */
-/*   Updated: 2022/12/08 19:54:12 by lde-ross         ###   ########.fr       */
+/*   Updated: 2022/12/09 09:58:11 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,22 @@ void sort_input(va_list args, char c)
 		ft_putstr_fd(va_arg(args, char *), 1);
 	else if (c == 'c')
 		ft_putchar_fd(va_arg(args, int), 1);
-	
+	else if (c == '%')
+		ft_putchar_fd(c, 1);
+}
+
+int	is_percent_char(char c)
+{
+	if (c == '%')
+		return (1);
+	return (0);
+}
+
+int is_percent(char a, char b)
+{
+	if(is_percent_char(a) && is_percent_char(b))
+		return (1);
+	return (0);
 }
 
 int	ft_print(const char *str, ...)
@@ -65,7 +80,7 @@ int	ft_print(const char *str, ...)
 	i = 0;
 	while (*str)
 	{
-		if (is_ph(*str, *(str + 1)))
+		if (is_ph(*str, *(str + 1)) || is_percent(*str, *(str + 1)))
 		{
 			sort_input(args, *(str + 1));
 			str += 2;
@@ -82,5 +97,6 @@ int	ft_print(const char *str, ...)
 int	main(void)
 {
 	char my_str[] = "This is a String!!!!!!!!!";
-	ft_print("h%iii%c %i, %s", -2147483648, 'y', 88, my_str);
+	ft_print("h%ii%si%c %i, %%i \n", -2147483648, my_str, 'y', 88);
+	printf("h%lii%si%c %i, %%i \n", -2147483648, my_str, 'y', 88);
 }
